864ff274efb9a8c03b7ee19d88d443d74d1261a6
wangxinwei <2087201681@qq.com>
2020-11-06 16:30:07 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "texst1.h"
+//#include "Graphics.h"
+int main() {
+    printf("Hello, Worl看看!\n");
+    printf("请输入1个数");
+    float r;
+    scanf("%f",&r);
+    printf("圆的面积为 %.2f\n",mianji(r));
+    return 0;
+}
---
+++ texst1.h
@@ -0,0 +1,15 @@
+//
+// Created by 86151 on 2020/9/16.
+//
+
+#ifndef UNTITLED4_TEXST1_H
+#define UNTITLED4_TEXST1_H
+
+#endif //UNTITLED4_TEXST1_H
+#define PI 3.14159
+
+float mianji(float r)
+
+{
+    return PI*r*r;
+}
